725430357aaa3e638b806c5e09445afb79e8375a
Michelle Racho <rachoma12@gmail.com>
2020-11-23 17:46:46 -0500

    adjust accordion transition

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const faqItem = document.querySelectorAll('.faq__item');
+const faqAnswer = document.querySelectorAll('.faq__answer');
 
-faqItem.forEach(item => {
-  item.addEventListener('click', () => toggleActive(item));
-});
-
-// toggle faq answer
 function toggleActive(target) {
   let question = target.querySelector('.faq__question');
   let answer = target.querySelector('.faq__answer');
@@ -13,4 +9,18 @@ function toggleActive(target) {
   question.classList.toggle('faq__question--active');
   answer.classList.toggle('faq__answer--active');
   showBtn.classList.toggle('show-btn--active');
+
+  answer.classList.contains('faq__answer--active')
+    ? (answer.style.maxHeight = `${answer.scrollHeight}px`)
+    : (answer.style.maxHeight = 0);
 }
+
+// toggle faq answer
+faqItem.forEach(item => {
+  item.addEventListener('click', () => toggleActive(item));
+});
+
+// prevent toggleActive event on answer click
+faqAnswer.forEach(answer => {
+  answer.addEventListener('click', e => e.stopPropagation());
+});
